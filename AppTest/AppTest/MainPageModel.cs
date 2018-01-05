f011b13e2a5caf9aa6f969d509c077aface525b2
Jose <Jose@DRAGON>
2018-01-05 07:50:39 +0100

    Add project files.

---
+++ AppTest/AppTest/MainPageModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppTest
+{
+    class MainPageModel
+    {
+
+        public string PNGImageUrl { get; set; }
+        public string WEBPImageUrl { get; set; }
+
+
+        public MainPageModel()
+        {
+            PNGImageUrl = "https://developers.google.com/chart/interactive/images/chart_printable.png";
+            WEBPImageUrl = "https://www.gstatic.com/webp/gallery/1.sm.webp";
+        }
+    }
+}
